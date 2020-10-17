e48879e879ed54b8a880657569fe1b1c8eb00c73
Brian Wu <wubrian@berkeley.edu>
2020-10-17 13:11:41 -0700

    hw3 progress

--- hw3/cs285/policies/argmax_policy.py
+++ hw3/cs285/policies/argmax_policy.py
@@ -18,6 +18,8 @@ class ArgMaxPolicy(object):
         print("qa_values.shape", qa_values.shape)
         print(type(qa_values))
         print(qa_values)
-        action = np.array(np.argmax(qa_values))
+        action = np.array([np.argmax(qa_values)])
         print(action)
-        return action.squeeze()
+        squeezed = action.squeeze()
+        print(squeezed)
+        return squeezed
