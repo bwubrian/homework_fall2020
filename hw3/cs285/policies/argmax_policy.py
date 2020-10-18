a0f04224819014eaa89c356a038af3b9322b21a4
Brian Wu <wubrian@berkeley.edu>
2020-10-18 01:11:44 -0700

    hw3 progress

--- hw3/cs285/policies/argmax_policy.py
+++ hw3/cs285/policies/argmax_policy.py
@@ -11,21 +11,10 @@ class ArgMaxPolicy(object):
             observation = obs
         else:
             observation = obs[None]
-        print(type(observation))
-        print(observation)
-        print(observation.shape)
         ## TODO return the action that maxinmizes the Q-value 
         # at the current observation as the output
         qa_values = self.critic.qa_values(observation)
-        #print("qa_values.shape", qa_values.shape)
-        #print(type(qa_values))
-        #print(qa_values)
-        print(type(qa_values))
-        print(qa_values)
-        print(qa_values.shape)
-
-        action = np.array([np.argmax(qa_values)])
-        
+        action = np.argmax(qa_values)
         print(action)
         #print(action)
         #squeezed = action.squeeze()
